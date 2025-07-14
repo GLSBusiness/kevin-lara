22fc568fbd3ae580c7a2f235644f1e4de9a60845
LaraVPN12 <kjlmenjivar@gmail.com>
2025-07-14 11:25:33 -0600

    feat: :white_check_mark: Add NewDetailScreen widget for displaying detailed news content

---
+++ lib/presentation/screens/new_detail_screen.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/material.dart';
+
+class NewDetailScreen extends StatelessWidget {
+  static const String name = 'new-detail-screen';
+  const NewDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
