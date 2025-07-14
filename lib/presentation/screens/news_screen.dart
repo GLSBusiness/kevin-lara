0e5c73c6b13c7378de2dc4e1b114dfab5cb9e0eb
LaraVPN12 <kjlmenjivar@gmail.com>
2025-07-14 14:30:42 -0600

    feat: :white_check_mark: Refactor NewsScreen to use ConsumerWidget and integrate newsProvider for state management

--- lib/presentation/screens/news_screen.dart
+++ lib/presentation/screens/news_screen.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class NewsScreen extends StatelessWidget {
+import '../providers/news_provider.dart';
+import '../widgets/widgets.dart';
+
+class NewsScreen extends ConsumerWidget {
   static const String name = 'news-screen';
   const NewsScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final newsAsync = ref.watch(newsProvider);
+
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      body: RefreshIndicator(
+        onRefresh: () => ref.refresh(newsProvider.future),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 24.0),
+          child: newsAsync.when(
+            data: (newsResponse) => NewsListWidget(articles: newsResponse.articles),
+            loading: () => const Center(child: CircularProgressIndicator()),
+            error: (error, stackTrace) =>
+                OnLoadErrorWidget(error: error, ref: ref),
+          ),
+        ),
+      ),
+    );
   }
 }
