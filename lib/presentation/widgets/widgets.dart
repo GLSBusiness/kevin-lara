7bb5d9981cec255134707bdc2a1c65bae3315121
LaraVPN12 <kjlmenjivar@gmail.com>
2025-07-14 15:07:24 -0600

    feat: :white_check_mark: Update widgets export to include widgets being used in the detail new screen

--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -1,3 +1,6 @@
 export 'news_list_widget.dart';
 export 'news_card_widget.dart';
 export 'on_load_error_widget.dart';
+export 'article_not_available_widget.dart';
+export 'image_not_supported_widget.dart';
+export 'image_not_found_widget.dart';
